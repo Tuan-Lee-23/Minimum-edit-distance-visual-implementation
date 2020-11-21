3020c9c6c732cbaa4a2a633ecafa331419821134
Tuan Le <sswmata123@gmail.com>
2020-11-21 20:37:50 +0700

    added traceback and fix time animation

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+from matplotlib.patches import Rectangle
 
+global ax
 
 def update_heatmap(data):
+    global ax
     ax = sns.heatmap(data, annot = True, cbar = False)
 
+def highlight(ax, y, x):
+    ax.add_patch(Rectangle((x, y), 1, 1, fill=True, edgecolor='cyan', lw=3))
 
 def animate_heat_map(str1, str2):
+    global ax
     fig = plt.figure(figsize = (12, 8))
 
     m, n = len(str1), len(str2)
@@ -28,6 +34,30 @@ def animate_heat_map(str1, str2):
 
     plt.tight_layout()
 
+def trace_back(data, m, n):
+    global ax
+
+    while (m != 0 or n != 0):
+        left = data[m][n - 1]
+        top = data[m - 1][n]
+        left_top = data[m - 1][n - 1]
+
+        min_square = min(left, top, left_top)
+        if left_top == min_square:
+            highlight(ax, m - 1, n - 1)
+            m -= 1
+            n -= 1
+        
+        elif left == min_square:
+            highlight(ax, m, n - 1)
+            n -= 1
+        
+        else:
+            highlight(ax, m - 1, n)
+            m -= 1
+
+        plt.pause(0.1)
+    return
 
  
 def editDistDP(str1, str2):
@@ -62,15 +92,22 @@ def editDistDP(str1, str2):
                 data[i][j] = 1 + min(data[i][j-1],        # Insert
                                    data[i-1][j],        # Remove
                                    data[i-1][j-1])    # Replace
-            update_heatmap(data)
+            
+            if j % 5 == 0:
+                update_heatmap(data)
+                plt.pause(0.1)
+                plt.clf()
 
             if i == m and j == n:
+                update_heatmap(data)
+                global ax
+                highlight(ax, m, n)
+                trace_back(data, m, n)
+
                 plt.pause(50)
-            plt.pause(0.000005)
-            plt.clf()    
-
-    goal = data[m][n] 
-
+                 
+            # plt.pause(0.00001) 
+            # plt.clf()  
 
 
 
